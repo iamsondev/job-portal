5a831059e7e25eb6c7bc1bcfa9301778fc2b6cb9
iamsondev <sondipkumarsk@gmail.com>
2025-06-18 12:51:31 +0600

    added footer from daisyui

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { Outlet } from 'react-router';
+import Navbar from '../Pages/shared/Navbar';
+import Footer from '../Pages/shared/Footer';
 
 const RootLayout = () => {
   return (
     <div>
+       <Navbar></Navbar>
          <Outlet></Outlet>
+         <Footer></Footer>
     </div>
   );
 };
