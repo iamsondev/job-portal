39a037b13908708f8a97ceb1acd8f478c6168c3d
iamsondev <sondipkumarsk@gmail.com>
2025-06-22 16:59:16 +0600

    added firebase and create context

--- src/Components/Register.jsx
+++ src/Components/Register.jsx
@@ -2,6 +2,13 @@ import Lottie from 'lottie-react';
 import React from 'react';
 import registerLottie from '../assets/register.json'
 const Register = () => {
+  const handleRegister = e => {
+    e.preventDefault();
+    const form = e.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log(email, password)
+  } 
   return (
     <div className="hero bg-base-200 min-h-screen">
       <div className="hero-content flex-col lg:flex-row-reverse">
@@ -11,15 +18,15 @@ const Register = () => {
         </div>
         <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
           <div className="card-body">
-            <fieldset className="fieldset">
+            <form onSubmit={handleRegister} className="fieldset">
               <h1 className='text-2xl font-bold'>Please Register</h1>
               <label className="label">Email</label>
-              <input type="email" className="input" placeholder="Email" />
+              <input type="email" name='email' className="input" placeholder="Email" />
               <label className="label">Password</label>
-              <input type="password" className="input" placeholder="Password" />
-              <div><a className="link link-hover">Forgot password?</a></div>
-              <button className="btn btn-neutral mt-4">Login</button>
-            </fieldset>
+              <input type="password" name='password' className="input" placeholder="Password" />
+            
+              <button className="btn btn-neutral mt-4">Register</button>
+            </form>
           </div>
         </div>
       </div>
--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,12 @@ import './index.css'
 import App from './App.jsx'
 import router from './Router/router.jsx'
 import { RouterProvider } from 'react-router'
+import AuthProvider from './Components/Context/AuthProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-      <RouterProvider router={router} />
+      <AuthProvider>
+           <RouterProvider router={router} />
+      </AuthProvider>
   </StrictMode>,
 )
