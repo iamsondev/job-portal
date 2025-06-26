9c45df066c7a84c65cd4e0daaff655666bcc3667
iamsondev <sondipkumarsk@gmail.com>
2025-06-26 23:56:33 +0600

    added asios for ui and sweetalert2

--- src/Pages/Home/JobApply.jsx
+++ src/Pages/Home/JobApply.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { useParams } from 'react-router';
 import useAuth from '../../useAuth/useAuth';
+import axios from 'axios';
+import Swal from 'sweetalert2';
+
 
 const JobApply = () => {
   const { id } = useParams();
@@ -13,7 +16,32 @@ const JobApply = () => {
     const linkedin = form.linkedin.value;
     const github = form.github.value;
     const resume = form.resume.value;
-    console.log(linkedin, github, resume)
+    console.log(linkedin, github, resume);
+
+    const application = {
+      id,
+      applicant: user.email,
+      linkedin,
+      github,
+      resume
+    }
+
+    axios.post('http://localhost:5000/applications', application)
+      .then(res => {
+        console.log(res.data)
+        if (res.data.insertedId) {
+          Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: "Your application has been submitted successfully",
+            showConfirmButton: false,
+            timer: 1500
+          });
+        }
+      })
+      .catch(error => {
+        console.log(error)
+      })
 
   }
 
@@ -23,7 +51,7 @@ const JobApply = () => {
       <h1 className='text-3xl '>Apply For This Job</h1>
       <form onSubmit={handleJobApply}>
         <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
-          
+
 
           <label className="label">Linked In</label>
           <input type="url" name='linkedin' className="input" placeholder="LinkedIn Profile url" />
@@ -35,7 +63,7 @@ const JobApply = () => {
           <input type="url" name='resume' className="input" placeholder="Resume Link" />
           <input className='btn' type="submit" value="Submit" />
         </fieldset>
-        
+
       </form>
     </div>
   );
