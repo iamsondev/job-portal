0aef28f6230e88818b5661d7c795836b78efe748
iamsondev <sondipkumarsk@gmail.com>
2025-06-26 17:38:24 +0600

    added useAuth, form

--- src/Pages/Home/JobApply.jsx
+++ src/Pages/Home/JobApply.jsx
@@ -1,9 +1,42 @@
 import React from 'react';
+import { useParams } from 'react-router';
+import useAuth from '../../useAuth/useAuth';
 
 const JobApply = () => {
+  const { id } = useParams();
+  const { user } = useAuth();
+  console.log(id, user)
+
+  const handleJobApply = e => {
+    e.preventDefault();
+    const form = e.target;
+    const linkedin = form.linkedin.value;
+    const github = form.github.value;
+    const resume = form.resume.value;
+    console.log(linkedin, github, resume)
+
+  }
+
+
   return (
     <div>
-           Aytasiii!!
+      <h1 className='text-3xl '>Apply For This Job</h1>
+      <form onSubmit={handleJobApply}>
+        <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+          
+
+          <label className="label">Linked In</label>
+          <input type="url" name='linkedin' className="input" placeholder="LinkedIn Profile url" />
+
+          <label className="label">Github</label>
+          <input type="url" name='github' className="input" placeholder="Github Link url" />
+
+          <label className="label">Resume</label>
+          <input type="url" name='resume' className="input" placeholder="Resume Link" />
+          <input className='btn' type="submit" value="Submit" />
+        </fieldset>
+        
+      </form>
     </div>
   );
 };
